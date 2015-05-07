3bab8844017dbbea3b71d1e65fc70150baf83b98
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-06 18:03:47 -0700

    added add node function, addNodeAtEndOfListDefinedByNode

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node Node;
 struct Node {
@@ -16,6 +17,7 @@ struct Node {
 
 void printListFromStartingNode(Node *thisNode);
 Node* searchListForIntFromStartingNode(int *query, Node *thisNode);
+void newNodeAtEndOfListDefinedByNode(Node* thisNode, int value);
 
 int main(int argc, const char * argv[]) {
     
@@ -32,13 +34,26 @@ int main(int argc, const char * argv[]) {
     
     printListFromStartingNode(&node0);
     
-    int query = 3;
+    newNodeAtEndOfListDefinedByNode(&node0, 4);
+    
+    int query = 4;
     Node* searchResult = searchListForIntFromStartingNode(&query, &node0);
     if (searchResult){
         printf("Search result is at list index: %d\n",(searchResult->value-1));
     } else{
         printf("Query is not present.");
     }
+    
+    newNodeAtEndOfListDefinedByNode(&node0, 5);
+    
+    query = 5;
+    searchResult = searchListForIntFromStartingNode(&query, &node0);
+    if (searchResult){
+        printf("Search result is at list index: %d\n",(searchResult->value-1));
+    } else{
+        printf("Query is not present.");
+    }
+
     
     return 0;
 }
@@ -60,3 +75,12 @@ Node* searchListForIntFromStartingNode(int *query, Node *thisNode){
     }
 }
 
+void newNodeAtEndOfListDefinedByNode(Node* thisNode, int value){
+    if (thisNode->next){
+        newNodeAtEndOfListDefinedByNode(thisNode->next, value);
+    } else {
+        thisNode->next = malloc(sizeof(Node));
+        thisNode->next->value = value;
+    }
+    
+}
